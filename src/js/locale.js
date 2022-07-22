3d05a728b58fc388e53daf8ec0085480978ce811
Giovanni Nunes <giovanni.nunes@gmail.com>
2022-07-21 21:19:30 -0300

    Fix a typo

--- src/js/locale.js
+++ src/js/locale.js
@@ -2,7 +2,7 @@ const ALL_MESSAGES = {
   "pt-BR": {
     "Something went wrong!": "Algo deu errado!",
     "Unable to load user information. Please, check if the name is correctly typed.":
-      "Não foi possível carregar as informações do usuário. Por favor, verique se o o nome foi digitado corretamentee.",
+      "Não foi possível carregar as informações do usuário. Por favor, verique se o o nome foi digitado corretamente.",
   },
 };
 
